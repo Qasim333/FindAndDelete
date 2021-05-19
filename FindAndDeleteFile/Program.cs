b11c39b2d4d914b927e72a0a12b8a34c16155ca1
Muhammad Qasim <qasim_ramzan@hotmail.com>
2021-05-19 16:05:55 +0500

    Delete Files
    Find and delete files, Files from folders and find files from sub folders.

---
+++ FindAndDeleteFile/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindAndDeleteFile
+{
+    class Program
+    {
+        //internal int static countd = 0;
+        static void Main(string[] args)
+        {
+            string path = "D:\\New folder\\New folder";
+            if (File.Exists(path))
+            {
+                // This path is a file
+                ProcessFile(path);
+            }
+            else if (Directory.Exists(path))
+            {
+                // This path is a directory
+                ProcessDirectory(path);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a valid file or directory.", path);
+            }
+            Console.WriteLine("Done");
+            Console.ReadLine();
+
+            //}
+        }
+
+
+        // Process all files in the directory passed in, recurse on any directories 
+        // that are found, and process the files they contain.
+        public static void ProcessDirectory(string targetDirectory)
+        {
+            // Process the list of files found in the directory.
+            string[] fileEntries = Directory.GetFiles(targetDirectory);
+            foreach (string fileName in fileEntries)
+                ProcessFile(fileName);
+
+            //Recurse into subdirectories of this directory.
+            string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+            foreach (string subdirectory in subdirectoryEntries)
+                ProcessDirectory(subdirectory);
+        }
+
+        // Insert logic for processing found files here.
+        public static void ProcessFile(string path)
+        {            
+            long length = new System.IO.FileInfo(path).Length;
+            if (length < 26100)
+            {                
+                File.Delete(path);                
+            }
+        }
+    }
+}
